d6c0a1a4674e51ec0ab2a762818f577673057251
Dipak Patil <dipakkumar2@gmail.com>
2024-05-28 15:48:00 +0530

    add hi

--- Helloworld.java
+++ Helloworld.java
@@ -3,5 +3,7 @@ class Helloworld
     public static void main(String args[])
     {
         System.out.println("Hello world");
+        System.out.println("Welcome");
+        System.out.println("HI");
     }
 }
